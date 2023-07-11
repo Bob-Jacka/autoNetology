ac6298e0331d79fdc0b72f135589b776d7bf68c1
Kirill <evilprog@yandex.ru>
2023-07-11 13:58:26 +0400

    junit4 added

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,47 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldReturnNineHundr() {
+        CashbackHackService chs = new CashbackHackService();
+        int act = chs.remain(100);
+        int exp = 900;
+        assertEquals(act, exp);
+    }
+
+    @Test
+    public void shouldReturnFiveHundr() {
+        CashbackHackService chs = new CashbackHackService();
+        int act = chs.remain(500);
+        int exp = 500;
+        assertEquals(act, exp);
+    }
+
+    @Test
+    public void shouldReturn999() {
+        CashbackHackService chs = new CashbackHackService();
+        int act = chs.remain(1);
+        int exp = 999;
+        assertEquals(act, exp);
+    }
+
+    @Test
+    public void shouldReturnWithZeroAmount() {
+        CashbackHackService chs = new CashbackHackService();
+        int act = chs.remain(0);
+        int exp = 1000;
+        assertEquals(act, exp);
+    }
+
+    @Test
+    public void shouldNotReturnWithBoundaryAmount() {
+        CashbackHackService chs = new CashbackHackService();
+        int act = chs.remain(1000);
+        int exp = 0;
+        assertEquals(act, exp);
+    }
+}
